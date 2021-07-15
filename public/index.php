028ee0e14e03d2d84cdfb30f4e21690def06d859
ricekot <ricekot@gmail.com>
2021-07-15 13:51:04 +0530

    Updates
    Add robots.txt
    Create src/index.php and move code block to it from src/sitemap.php
    Lint code

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 $app = AppFactory::create();
-
 $app->addRoutingMiddleware();
 
-require __DIR__ . '/../src/app/sitemap.php';
-require __DIR__ . '/../src/app/vulnerabilities/host-header-injection.php';
-require __DIR__ . '/../src/app/vulnerabilities/GhostCatCVE20201938.php';
+require __DIR__.'/../src/app/index.php';
+require __DIR__.'/../src/app/robots.php';
+require __DIR__.'/../src/app/sitemap.php';
+require __DIR__.'/../src/app/vulnerabilities/host-header-injection.php';
+require __DIR__.'/../src/app/vulnerabilities/GhostCatCVE20201938.php';
 
 $app->run();
