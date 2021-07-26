48317bf7b0efe4283a311d60a39a1d7538a80415
Prince Mendiratta <prince.mendi@gmail.com>
2021-07-26 12:30:34 +0530

    Better check optimization
    Signed-off-by: Prince Mendiratta <prince.mendi@gmail.com>

--- src/app/vulnerabilities/Unauthenticated Gitlab SSRF CVE 2021-22214.php
+++ src/app/vulnerabilities/Unauthenticated Gitlab SSRF CVE 2021-22214.php
@@ -21,15 +21,15 @@ $app->post(
         $_content = $_obj->content;
         
         if(!empty($_body)){
-            try{
-                $_obj = json_decode($_body, FALSE);
-                $_content = $_obj->content;
+            $_obj = json_decode($_body, FALSE);
+            $_content = $_obj->content;
+            if(isset($_content)){
                 $_resp='{"status": "valid","errors": [],"warnings": [],"merged_yaml": "API\n:.api_test:\n  :rules:\n  - :if: $CI_PIPELINE_SOURCE==\"merge_request_event\"\n    :changes:\n    - src/api/*\n:deploy:\n  :rules:\n  - :when: manual\n    :allow_failure: true\n  :extends:\n  - \".api_test\"\n  :script:\n  - echo \"hello world\"" }';
                 $response->getBody()->write($_resp);
                 return $response->withHeader("content-type", "application/json")
                                 ->withStatus(200);
             }
-            catch(Exception $e){
+            else{
                 $response->getBody()->write('{"error": "404 Not Found"}');
                 return $response->withHeader("content-type", "application/json")
                                 ->withStatus(404);
