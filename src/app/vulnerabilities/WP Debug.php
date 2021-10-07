697d34b5a9df97b3d772fa83b9e2ec1cedd18a99
Karthik UJ <karthikuj2001@gmail.com>
2021-10-07 17:52:03 +0530

    Adds AWStats script vuln and WP-Debug enabled vuln
    Signed-off-by: Karthik UJ <karthikuj2001@gmail.com>

---
+++ src/app/vulnerabilities/WP Debug.php
@@ -0,0 +1,14 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app->get(
+    '/wp-debug/',
+    function (Request $request, Response $response) {
+        $response->getBody()->write('<b>Parse error</b> missing \')\' on line <b>189</b>');
+        return $response->withHeader("content-type", "text/html")
+                        ->withStatus(200);
+    }
+);
