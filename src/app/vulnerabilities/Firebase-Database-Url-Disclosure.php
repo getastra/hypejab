d478a2689590372e7ce6df178ade5c00c0f282ce
iamsonidarshan <iamsonidarshan@gmail.com>
2021-10-14 17:44:23 -0400

    small changes

--- src/app/vulnerabilities/Firebase-Database-Url-Disclosure.php
+++ src/app/vulnerabilities/Firebase-Database-Url-Disclosure.php
@@ -7,7 +7,7 @@ use Slim\Factory\AppFactory;
 $app->get(
     '/firebase-db',
     function (Request $request, Response $response) {
-        $response->getBody()->write('<p>Crawl me:  https://test-project-7a1a0-default-rtdb.firebaseio.com/ </p>');
+        $response->getBody()->write('<p>Detect me:  https://test-project-7a1a0-default-rtdb.firebaseio.com/ </p>');
         return $response->withHeader("content-type", "text/html")
                         ->withStatus(200);
     }
