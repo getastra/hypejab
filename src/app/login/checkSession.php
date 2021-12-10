558cffa5a7368399c069228eb7a66e34a86eb74b
iamsonidarshan <iamsonidarshan@gmail.com>
2021-12-10 15:40:32 +0530

    reverting unauth access (#34)
    Co-authored-by: Darshan Soni <darshan.soni@getastra.com>

--- src/app/login/checkSession.php
+++ src/app/login/checkSession.php
@@ -1,9 +1,9 @@
 <?php
 
-//session_start();
-//if (!isset($_SESSION['user'])) {
-//    header("Location: /hypejablogin");
-//    die();
-//}
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: /hypejablogin");
+    die();
+}
 
 ?>
