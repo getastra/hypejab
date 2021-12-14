abb9b934bd80ff9e83bca12ab06e47e61ae95b62
Karthik UJ <karthikuj2001@gmail.com>
2021-12-14 13:39:26 +0530

    Updates Log4j RCE vuln to log GET query too
    Signed-off-by: Karthik UJ <karthikuj2001@gmail.com>

--- src/app/vulnerabilities/Log4j RCE.php
+++ src/app/vulnerabilities/Log4j RCE.php
@@ -5,16 +5,56 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app->get(
+    '/log4j?val=boat',
+    function (Request $request, Response $response, $args) {
+
+        $query = $request->getQueryParams()['val'];
+        echo $query;
+        $data = "GET Log4j in use";
+
+        if (strpos($query, 'jndi') !== false) {
+            $pos = strpos($query,"boat.") + 5;
+            $domain = $domain = substr($query, $pos, -3);
+            echo $domain;
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, "http://".$domain);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+        }
+
+        $response->getBody()->write($data);
+        return $response->withHeader("content-type", "text/html")
+                        ->withStatus(200);
+    }
+);
+
+$app->get(
     '/log4j',
     function (Request $request, Response $response) {
 
         $header = $request->getHeader('User-Agent');
-        print_r($header);
-        $domain = substr($header[0], 13, -1);
-        echo $domain;
+        $query = $request->getQueryParams()['val'];
+        echo $query;
         $data = "Log4j in use";
         
         if (strpos($header[0], 'jndi') !== false) {
+            $domain = substr($header[0], 13, -1);
+            echo $domain;
+
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_URL, "http://".$domain);
+            $data = curl_exec($ch);
+            curl_close($ch);
+
+        } else if (strpos($query, 'jndi') !== false) {
+            $pos = strpos($query,"val.");
+            $domain = $domain = substr($query, $pos, -1);
+            echo $domain;
+
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
             curl_setopt($ch, CURLOPT_URL, "http://".$domain);
