b6d9d540468472aa2f115ef7120148586ce0d79d
Karthik UJ <karthikuj2001@gmail.com>
2022-01-06 12:31:52 +0530

    Minor changes
    Signed-off-by: Karthik UJ <karthikuj2001@gmail.com>

--- src/app/vulnerabilities/Dangerous JS Functions.php
+++ src/app/vulnerabilities/Dangerous JS Functions.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app->get(
-    '/fp/dangerousJSFunctions',
+    '/dangerousJSFunctions',
     function (Request $request, Response $response) {
         $response->getBody()->write('<html>
 <head>
