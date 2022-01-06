91e3eb4abf48b5e7a1ccdd6fc945f51fd8d70077
Karthik UJ <karthikuj2001@gmail.com>
2022-01-06 12:21:58 +0530

    Updates hypejab to v1.0.4
    Signed-off-by: Karthik UJ <karthikuj2001@gmail.com>

--- public/index.php
+++ public/index.php
@@ -54,7 +54,9 @@ require __DIR__ . '/../src/app/vulnerabilities/Flask Debug Mode.php';
 require __DIR__ . '/../src/app/vulnerabilities/Drupal Backup Migrate.php';
 require __DIR__ . '/../src/app/vulnerabilities/Log4j RCE.php';
 require __DIR__ . '/../src/app/vulnerabilities/X-Powered-By Header.php';
+require __DIR__ . '/../src/app/vulnerabilities/Dangerous JS Functions.php';
 require __DIR__ . '/../src/app/vulnerabilities/FP/X-Powered-By Header.php';
+require __DIR__ . '/../src/app/vulnerabilities/FP/Dangerous JS Functions.php';
 
 // Define Custom Error Handler
 $customErrorHandler = function (
