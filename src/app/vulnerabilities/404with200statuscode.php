82a042c295a64e3daaa5ea8b135a9b29cc9f50c8
aryangupta701 <garyan447@gmail.com>
2023-09-01 13:53:44 +0530

    added some more pages
    Signed-off-by: aryangupta701 <garyan447@gmail.com>

---
+++ src/app/vulnerabilities/404with200statuscode.php
@@ -0,0 +1,14 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app->get(
+    '/page-not-found[/{segments:.*}]',
+    function (Request $request, Response $response) {
+        $response->getBody()->write('404 Not Found');
+        return $response->withHeader("content-type", "text/html")
+                        ->withStatus(200);
+    }
+);
