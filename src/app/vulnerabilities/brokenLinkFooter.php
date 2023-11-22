d9db2373c0e27233220774c49134c19247eb8fe1
aryangupta701 <garyan447@gmail.com>
2023-11-22 09:57:20 +0530

    add broken link footer
    Signed-off-by: aryangupta701 <garyan447@gmail.com>

---
+++ src/app/vulnerabilities/brokenLinkFooter.php
@@ -0,0 +1,14 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app->get(
+    '/broken-footer',
+    function (Request $request, Response $response) {
+        $response->getBody()->write('<footer><p>https://www.youtube.com/@KaanPhodMusic<p></footer>');
+        return $response->withHeader("content-type", "text/html")
+        ->withStatus(200);
+    }
+);
