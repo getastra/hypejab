dc68826aab4f44845579b5f339f94bbdc56b13ad
aryangupta701 <garyan447@gmail.com>
2023-11-22 10:53:11 +0530

    implement rate limit in forgot password
    Signed-off-by: aryangupta701 <garyan447@gmail.com>

--- public/index.php
+++ public/index.php
@@ -95,6 +95,7 @@ require __DIR__ . '/../src/app/vulnerabilities/httpVerbTempering.php';
 require __DIR__ . '/../src/app/vulnerabilities/fileUpload.php';
 require __DIR__ . '/../src/app/vulnerabilities/serializedData.php';
 require __DIR__ . '/../src/app/vulnerabilities/brokenLinkFooter.php';
+require __DIR__ . '/../src/app/vulnerabilities/resetPassword.php';
 
 
 // False positives section
