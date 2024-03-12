8bddea4bd2651381411da1edbb8244c3b2455003
divyansh jain <divyansh.jain@getastra.com>
2024-03-12 09:49:29 +0530

    improvement in user email leaks
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>

--- src/app/vulnerabilities/emailLeaks.php
+++ src/app/vulnerabilities/emailLeaks.php
@@ -9,9 +9,9 @@ $app->get(
     function (Request $request, Response $response) {
         $response->getBody()->write('
             <p>testmail@hypejab.com</p>
-            <p>adminmail@hypejab.com</p>
-            <p>superadmin@hypejab.com</p>
-            <p>adminmail@hypejab.com</p>');
+            <p>somemail@hypejab.com</p>
+            <p>superman@hypejab.com</p>
+            <p>astranaut@hypejab.com</p>');
         return $response->withHeader("content-type", "text/html")
                         ->withStatus(200);
     }
