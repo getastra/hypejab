71db15c93052aee2c285728e955b6c421211394d
divyanshjainATastra <147395972+divyanshjainATastra@users.noreply.github.com>
2024-06-07 11:03:56 +0530

    info disclosure (#116)
    * info disclosure
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    * info disclosure update
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    * update
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    * more update
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    * base64 pages
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    * base64 pages improvement
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>
    
    ---------
    
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>

---
+++ src/app/vulnerabilities/info-disclosure.php
@@ -0,0 +1,25 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+$app->get(
+    '/info-disclosure?PAN=QLQPS1836D',
+    function (Request $request, Response $response) {
+        $response->getBody()->write('
+        Info disclosure in url');
+        return $response->withHeader("content-type", "text/html")
+                        ->withStatus(200);
+    }
+);
+
+$app->get(
+    '/info-disclosure',
+    function (Request $request, Response $response) {
+        $response->getBody()->write('
+        Info disclosure in url');
+        return $response->withHeader("content-type", "text/html")
+                        ->withStatus(200);
+    }
+);
