e379efd844e6c603770fafd1e38136aa921b13f0
divyansh jain <divyansh.jain@getastra.com>
2024-07-01 13:02:53 +0530

    update
    Signed-off-by: divyansh jain <divyansh.jain@getastra.com>

--- src/app/vulnerabilities/polyfill.php
+++ src/app/vulnerabilities/polyfill.php
@@ -5,7 +5,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 $app->get('/polyfill', function (Request $request, Response $response) {
-    $response->getBody()->write("<script src=https://cdn.polyfill.io></script><h1>helloWorld<h1>");
+    $response->getBody()->write("<script src=https://bootcdn.net/polyfill.js></script><h1>helloWorld<h1>");
     return $response->withHeader("content-type", "text/html")
                     ->withStatus(200);
 });
